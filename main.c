70b5e5a927ceb29d89d35a11e68caef2c88c5cba
Md. Abdullah-Al-Bakee <mail.bakee@gmail.com>
2022-11-26 07:50:04 +0100

    Add xcode project, works with hard coded 5 byte data and localhost

--- main.c
+++ main.c
@@ -11,7 +11,7 @@
 #define FALSE 0
 
 typedef struct {
-    char* ip;
+    char ip[16];
     int port;
 } socket_data;
 
@@ -32,7 +32,6 @@ int get_server_socket() {
 
 socket_data get_invalid_socket_data() {
     socket_data peer_socket_data;
-    peer_socket_data.ip = "";
     peer_socket_data.port = -1;
 
     return peer_socket_data;
@@ -54,7 +53,6 @@ socket_data parse_server_response(const char* server_response, int peer_id) {
     // parsing...
     // 3:111.222.33.44:5678
     char temp_client_id[10];
-    char temp_ip_address[16];
     char temp_port[10];
     const char seperator = ':';
 
@@ -80,13 +78,13 @@ socket_data parse_server_response(const char* server_response, int peer_id) {
         printf("Body pointer: %p\r\n", (void*)body_pointer);
         separator_position = strchr(body_pointer, (int)seperator);
         printf("Seperator position: %p\r\n", (void*)separator_position);
-        memset(temp_ip_address, 0, 16);
+        memset(data.ip, 0, 16);
         Print("Memset done!");
         uint64_t sp = (uint64_t)separator_position;
         uint64_t bp = (uint64_t)body_pointer;
         uint64_t temp_length2 = sp - bp;
         printf("Temp length: %llu", temp_length2);
-        memcpy(data.ip, temp_ip_address, temp_length);
+        memcpy(data.ip, body_pointer, temp_length2);
         body_pointer = separator_position + 1;
         Print("IP done.");
 
@@ -118,18 +116,24 @@ socket_data parse_server_response(const char* server_response, int peer_id) {
     return data;
 }
 
-
-socket_data get_peer_socket_data(int server_socket, user_options options) {
-    socket_data peer_socket_data;
-    peer_socket_data.ip = "";
-    peer_socket_data.port = -1;
-
+int make_server_call(int server_socket, user_options options) {
     char http_body[200];
     memset(http_body, 0, 200);
     sprintf(http_body, "GET /?client-id=%d HTTP/1.1\r\nHost: punch.onlinewebshop.net \r\n\r\n", options.my_id);
     int bytes_written = tcp_socket_send(server_socket, http_body, strlen(http_body));
     if(bytes_written <= 0) {
         printf("Could not send data over socket.");
+        return -1;
+    }
+    
+    return 0;
+}
+
+socket_data get_peer_socket_data(int server_socket, user_options options) {
+    socket_data peer_socket_data = get_invalid_socket_data();
+    
+    int response = make_server_call(server_socket, options);
+    if(response < 0) {
         return peer_socket_data;
     }
 
@@ -179,8 +183,8 @@ void handle_as_client(int socketfd, user_options options, socket_data media_serv
     int media_server_socketfd;
     // try connecting to the server
     while(TRUE) {
-        printf("Connecting to media server...\r\n");
-        media_server_socketfd = tcp_socket_connect(media_server_data.ip, media_server_data.port, 10);
+        printf("Connecting to media server: %s:%d\r\n", media_server_data.ip, media_server_data.port);
+        media_server_socketfd = tcp_socket_connect(/*media_server_data.ip*/"127.0.0.1", media_server_data.port, 10);
         if(media_server_socketfd > 0) {
             break;
         }
@@ -213,9 +217,18 @@ void to_upper_case(char* str, const int str_length) {
 }
 
 void handle_as_server(int socketfd, user_options options) {
+    int response = make_server_call(socketfd, options);
+    if(response < 0) {
+        Print("Server request failed!");
+        return;
+    }
+    
+    Print("Server request successful!");
+    
     int local_port = tcp_get_local_port(socketfd);
     // bind/listen
     int server_socketfd = tcp_socket_listen("0.0.0.0", local_port);
+    printf("Listening on port: %d\r\n", local_port);
 
     if(server_socketfd < 0) {
         printf("Error in listening...\r\n");
@@ -225,22 +238,35 @@ void handle_as_server(int socketfd, user_options options) {
     printf("Listen complete!\r\n");
 
     // accept
+    Print("Waiting for a connection.");
     char remote_ip[20];
     int remote_port;
-    int client_socketfd = tcp_socket_accept(server_socketfd, remote_ip, &remote_port, 10);
+    int client_socketfd = tcp_socket_accept(server_socketfd, remote_ip, &remote_port, 3600);
+    if(client_socketfd < 0) {
+        printf("Error occured while accepting a connection. %d\r\n", client_socketfd);
+        return;
+    }
 
+    printf("Accepted a connection %d\r\n", client_socketfd);
     // reply in a loop, break in special case
     const int read_buffer_length = 100;
     char read_buffer[read_buffer_length];
     char write_buffer[read_buffer_length];
 
     while(TRUE) {
-        int read_length = tcp_socket_pull(client_socketfd, read_buffer, read_buffer_length, 2);
-        if(read_length <= 0) continue;
+        Print("Pulling from client socket");
+        int read_length = tcp_socket_pull(client_socketfd, read_buffer, 5, 2);
+        if(read_length <= 0) {
+            Print("No data read");
+            continue;
+        }
 
+        printf("Got data from client: %s\r\n", read_buffer);
         memcpy(write_buffer, read_buffer, read_length);
         to_upper_case(write_buffer, read_length);
         
+        Print("Sending response to client");
+        printf("Response: %s\r\n", write_buffer);
         tcp_socket_send(client_socketfd, write_buffer, read_length);
     }
 }
@@ -281,7 +307,7 @@ int main(int argc, char* argv[]) {
             return -1;
         }
         Print("Got peer socket data");
-        printf("Peer ip: %s, port: %d", media_server_data.ip, media_server_data.port);
+        printf("Peer ip: %s, port: %d\r\n", media_server_data.ip, media_server_data.port);
         handle_as_client(server_socket, options, media_server_data);
     }
 
